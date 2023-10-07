1e779bb63d0bf6f26b3710f499665e2b6b58653c
Tiago Silva Cardoso - 06453943514 <tiago.s.cardoso@animaeducacao.com.br>
2023-10-07 11:54:59 -0300

    feat/add user reducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import "./App.css";
-import Header from "./components/header";
-import Products from "./components/products";
+import './App.css';
+import Header from './components/header';
+import Products from './components/products';
 
 const App = () => {
   return (
